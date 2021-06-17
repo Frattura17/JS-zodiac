0b7810286e4c3adeb8473c4cc4a7251362618f0c
Emmanuel <emmanuelfra02@hotmail.com>
2021-06-17 18:26:26 -0300

    17/06 WHILE and FOR

---
+++ js/01-FOR.js
@@ -0,0 +1,7 @@
+console.log("/Ejercicio 1/")
+for(let i = 0 ; i < 10; i += 1){
+    console.log("entró")
+}
+    console.log("salio")
+
+console.log("/Ejercicio 2/")
--- js/Bucles.js
+++
@@ -1,5 +0,0 @@
-
-for(let i = 0 ; i < 10; i += 1){
-    console.log("entró")
-}
-    console.log("salio")
